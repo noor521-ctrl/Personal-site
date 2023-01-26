0a6d8ac8183ff53118a41b3ef4bf11e4f8c0e578
Noor Al-masry <83882039+noor521-ctrl@users.noreply.github.com>
2023-01-26 21:46:15 +0200

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,99 @@
+// $(document).ready(function(){
+        
+
+
+// })
+
+// var oldPrice=200,
+//     newPrice=130,
+//     result=oldPrice-newPrice;
+//     console.log(result);
+// var one= 450,
+//     two= false;
+// if (two==false){
+//     var one = 350;
+// }
+// console.log(one);
+// var user=[1,"Noor",6,7,9,3];
+// console.log(user[1]);
+// var student = {
+//     name :"noor",
+//     age : 22,
+//     color: ["red","yellow"],
+
+// }
+// console.log(student.color[1])
+    // var myName="Noor",
+    //     myAge=22;
+    // document.getElementById("test").innerHTML =
+    // "myName: " +"" + myName + "<br>" +
+    // "myAge: " +"" + myAge + "<br>";
+
+// var mobileprice=1200;
+// if(mobileprice<1500){
+//     console.log("نعم");
+// }else{
+//     console.log("لا");
+// }
+// var myprice=60;
+
+// if(myprice==="nooor"){
+//     console.log("T");
+// }else{
+//     console.log("F");
+// }
+// var myprice = 60,
+//     age = 20,
+//     address = 30;
+// if( myprice == 60 || age == 40 || address == 30){
+//     alert("صح");
+// }else{
+//     alert("غلط");
+// }
+// var myprice = 60,
+//     age = 20,
+//     address = 30;
+// if( myprice == 60 && age == 40 && address == 30){
+//     alert("صح");
+// }else{
+//     alert("غلط");
+// }
+// function noor(){
+//     console.log("noor" );
+
+// }
+// $("button").on("click", function(){
+//     noor();
+// } )
+// var myName="noor";
+// console.log(myName);
+// function Hey(){
+//     "use strict";
+//     var myName="boi";
+//     if(myName=="boi"){
+//         alert("Hello " + myName);
+//     }
+
+// }
+// Hey();
+// function myInfo(){
+//     "use strict";
+//     var Name = "Noor",
+//         age = "22";
+//         return Name;
+// }
+// document.getElementById("test").innerHTML= myInfo();
+// function myInfo(name,age){
+//     "use strict";
+//     return age* 365 + "myname= " + "name";
+// }
+
+
+
+
+
+
+
+
+
+
